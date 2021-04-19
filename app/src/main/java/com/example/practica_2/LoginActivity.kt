90baef8aec2fc02f1fe9f0c1bd8b01619c409328
Judith Vanessa Mallama Pascumal <judith.mallama@udea.edu.co>
2021-04-19 17:04:50 -0500

    Avance two

--- app/src/main/java/com/example/practica_2/LoginActivity.kt
+++ app/src/main/java/com/example/practica_2/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.example.practica_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.practica_2.databinding.ActivityLogin2Binding
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class LoginActivity : AppCompatActivity()
+{
+    private lateinit var login2Binding: ActivityLogin2Binding
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+        login2Binding = ActivityLogin2Binding.inflate(layoutInflater)
+        setContentView(login2Binding.root)
+
     }
 }
--- app/src/main/java/com/example/practica_2/SplashActivity.kt
+++ app/src/main/java/com/example/practica_2/SplashActivity.kt
@@ -2,10 +2,26 @@ package com.example.practica_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import java.sql.Time
+import java.util.*
+import kotlin.concurrent.timerTask
 
-class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class SplashActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_splash)
+
+        val timer = Timer()
+
+
+
+
+
+
+
     }
 }
